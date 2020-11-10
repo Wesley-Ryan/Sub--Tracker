0fc878a64001e75cb4bf6d7cb8936d67d055ac37
Wesley-Ryan <im.wesleyryan@gmail.com>
2020-11-09 20:18:24 -0800

    APP

---
+++ src/components/SubscriptionCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const SubscriptionCard = () => {
+    return ( 
+        <div>
+            Card here!
+        </div>
+     );
+}
+ 
+export default SubscriptionCard;
---
+++ src/components/SubscriptionList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const SubscriptionList = () => {
+    return (  
+        <div>
+            List of Subs
+        </div>
+    );
+}
+ 
+export default SubscriptionList
